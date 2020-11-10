336758b13a2cb804c4989991c3c4dac8d5a48e31
yananlz <15403356239@qq.com>
2020-11-10 19:37:25 +0800

    Signed-off-by: yananlz <15403356239@qq.com>

---
+++ build/precache-manifest.806ac6709f63f3b877ddef6fe7807518.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a28309145ef1a8c4347363a17a8636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9847280bcc3f7d068ebf",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "ad008d591b5d8e592abf",
+    "url": "/static/css/main.725d6279.chunk.css"
+  },
+  {
+    "revision": "9847280bcc3f7d068ebf",
+    "url": "/static/js/2.6c251f35.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "/static/js/2.6c251f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad008d591b5d8e592abf",
+    "url": "/static/js/main.015cede7.chunk.js"
+  },
+  {
+    "revision": "7329818627515a18dae5",
+    "url": "/static/js/runtime-main.581274c4.js"
+  },
+  {
+    "revision": "347fdcd2146f2beeca62c918ff71dff9",
+    "url": "/static/media/lunbotu2.347fdcd2.jpg"
+  },
+  {
+    "revision": "93290ec560183def4a0bfc1ced999728",
+    "url": "/static/media/lunbotu3.93290ec5.jpg"
+  },
+  {
+    "revision": "f3572b071ace0e064c1a7709200e1e32",
+    "url": "/static/media/lunbotu5.f3572b07.jpg"
+  },
+  {
+    "revision": "7ea9c5d2f6875a66c6c20a2880541143",
+    "url": "/static/media/lunbotu6.7ea9c5d2.jpg"
+  },
+  {
+    "revision": "5aec6f2468c6cbcfbfc4a48cb795a9fc",
+    "url": "/static/media/lunbotu7.5aec6f24.jpg"
+  },
+  {
+    "revision": "74dde724fa456ccee43170a7298c1401",
+    "url": "/static/media/lunbotu8.74dde724.jpg"
+  }
+]);
